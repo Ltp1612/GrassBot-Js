85c134968c9a3bc1dac1fe04585c86381f00567b
Gangster <40878621+GangsterGpc@users.noreply.github.com>
2024-12-07 04:26:29 +0630

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,10 +1,8 @@
 class Config {
   constructor() {
     this.ipCheckURL = 'https://ipinfo.io/json';
-    this.wssList = ['proxy.wynd.network:4444', 'proxy.wynd.network:4650'];
+    this.wssHost = 'proxy.wynd.network:4444';
     this.retryInterval = 20000;
-    this.wssHost =
-      this.wssList[Math.floor(Math.random() * this.wssList.length)];
   }
 }
 
